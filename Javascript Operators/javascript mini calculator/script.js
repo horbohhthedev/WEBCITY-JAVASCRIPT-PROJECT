f1d5ed7470b83394e3bf837a03b2b9841f885b0d
honourthatdev <honour0104@gmail.com>
2024-10-18 05:22:21 -0700

    in progress

--- Javascript Operators/javascript mini calculator/script.js
+++ Javascript Operators/javascript mini calculator/script.js
@@ -1,16 +1,57 @@
-        var n1 = document.getElementById("num1");
-        var n2 = document.getElementById("num2");
-        var res = document.getElementById("result");
+// MAIN OPERATORS
+function myFunction(){
 
-        document.getElementById("add").addEventListener("click", function() {
-            res.value = parseInt(n1.value)+parseInt(n2.value)
-        } )
-        document.getElementById("sub").addEventListener("click", function() {
-            res.value = parseInt(n1.value)-parseInt(n2.value)
-        } )
-        document.getElementById("div").addEventListener("click", function() {
-            res.value = parseInt(n1.value)/parseInt(n2.value)
-        } )
-        document.getElementById("mul").addEventListener("click", function() {
-            res.value = parseInt(n1.value)*parseInt(n2.value)
-        } )
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    let calculate = document.getElementById('operator').value;
+
+
+    if( calculate==='+'){
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('result').value=num1+num2;
+    }
+
+    if( calculate==='-'){
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('result').value=num1-num2;
+    }
+
+    if( calculate==='/'){
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('result').value=num1/num2;
+    }
+
+    if( calculate==='*'){
+        let num1 = parseInt(document.getElementById('num1').value);
+        let num2 = parseInt(document.getElementById('num2').value);
+        document.getElementById('result').value=num1*num2;
+    }
+   
+
+   
+
+}
+
+ // MOBILE DEVICE BUTTON
+
+
+ var n1 = document.getElementById("num1");
+ var n2 = document.getElementById("num2");
+ var res = document.getElementById("result");
+
+ document.getElementById("add2").addEventListener("click", function() {
+     res.value = parseInt(n1.value)+parseInt(n2.value)
+ } )
+ document.getElementById("sub3").addEventListener("click", function() {
+     res.value = parseInt(n1.value)-parseInt(n2.value)
+ } )
+ document.getElementById("div4").addEventListener("click", function() {
+     res.value = parseInt(n1.value)/parseInt(n2.value)
+ } )
+ document.getElementById("mul5").addEventListener("click", function() {
+     res.value = parseInt(n1.value)*parseInt(n2.value)
+ } )
+
